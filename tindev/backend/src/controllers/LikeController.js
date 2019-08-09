441dfdf85e77ad3daf242d9116ac09a13f83839c
Sam Velasquez <atrox.lapa@hotmail.com>
2019-08-08 22:48:04 -0300

    feat: added backend functionalities

---
+++ tindev/backend/src/controllers/LikeController.js
@@ -0,0 +1,8 @@
+const Dev = require('../models/Dev')
+
+module.exports = {
+    store(req, res) {
+        console.log(req.params.devId)
+        return res.json({ ok: true })
+    }
+}
--- tindev/backend/src/models/Dev.js
+++ tindev/backend/src/models/Dev.js
@@ -1,21 +1,28 @@
-const  {Schema, model} = require('mongoose')
+const { Schema, model } = require('mongoose')
 const DevSchema = new Schema({
-    name:{
+    name: {
         type: String,
-        required:true
+        required: true
     },
-    user:{
+    user: {
         type: String,
         required: true
     },
     bio: String,
-    avatar:{
+    avatar: {
         type: String,
-        required:true
-    }
-
-},{
-    timestamps:true
-})
+        required: true
+    },
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev'
+    }],
+    dislikes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev'
+    }]
+}, {
+        timestamps: true
+    })
 
 module.exports = model('Dev', DevSchema)
